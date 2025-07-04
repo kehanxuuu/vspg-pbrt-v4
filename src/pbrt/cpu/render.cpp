8f3213da28d0dee5604b39c174ed147ec4401229
pbrt4bounty <73945652+pbrt4bounty@users.noreply.github.com>
2025-07-04 20:22:20 +0200

    Remove the warning and allow 'guidedvolpathvspg' integrator to use GuidedGBufferFilm

--- src/pbrt/cpu/render.cpp
+++ src/pbrt/cpu/render.cpp
@@ -87,9 +87,11 @@ void RenderCPU(BasicScene &parsedScene) {
         Warning("GBufferFilm is not supported by the \"%s\" integrator. The channels "
                 "other than R, G, B will be zero.",
                 parsedScene.integrator.name);
-    if (film.Is<GuidedGBufferFilm>() && !(parsedScene.integrator.name == "guidedpath" ||
-                                parsedScene.integrator.name == "guidedvolpath"))
-    Warning("GuidedGBufferFilm is not supported by the \"%s\" integrator. The channels "
+    if (film.Is<GuidedGBufferFilm>() &&
+        !(parsedScene.integrator.name == "guidedpath" ||
+          parsedScene.integrator.name == "guidedvolpath" ||
+          parsedScene.integrator.name == "guidedvolpathvspg"))
+        Warning("GuidedGBufferFilm is not supported by the \"%s\" integrator. The channels "
             "other than R, G, B will be zero.",
             parsedScene.integrator.name);
 
